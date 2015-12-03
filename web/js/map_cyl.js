aaff9a0708da5aa223ad92bb6dc9cc56472edc0d
Sergio García <garciparedes@gmail.com>
2015-12-03 10:56:00 +0100

    Added Latex and web files

---
+++ web/js/map_cyl.js
@@ -0,0 +1,6 @@
+function changeColor(){
+    var elem = document.getElementById('map_cyl');
+    //var svgDoc = elem.contentDocument;
+    var elem2 = document.getElementById('leon');
+    elem2.style.fill = 'rgb(0,0,0)';
+}
