8f7e7e17f3ff6b7a30c82b791acfadabd94404ea
Sergio García <garciparedes@gmail.com>
2015-12-05 14:41:46 +0100

    Worked on CSV file extraction

--- manipulateData/js/extract.js
+++ manipulateData/js/extract.js
@@ -1,10 +1,40 @@
 var fileName ="Indicadores_medioambientales.csv";
+
+
 function extract(){
 
-    d3.csv("Indicadores_medioambientales.csv", function(data) {
+    d3.csv(fileName, function(data) {
         var len = data.length;
+        var indicadorList = new Array();
+
         for (i = 0; i < len; i++){
-            console.log(data[i].Indicador);
+
+            if (indicadorList[data[i].Indicador] == undefined){
+                indicadorList[data[i].Indicador] = new Array();
+            }
+
+            indicadorList[data[i].Indicador].push(
+                    {
+                        Provincia: data[i].Provincia,
+                        FechaValidez: data[i].FechaValidez,
+                        Valor: data[i].Valor,
+                        Unidad: data[i].Unidad,
+                        Frecuencia: data[i].Frecuencia
+                    }
+            );
+
         }
+
+        for (var key in indicadorList){
+            console.log(indicadorList[key]);
+        }
+
+        /*
+        indicadorList ["hola"] = "Chanchanchan";
+        console.log(indicadorList["adios"]);
+        if (indicadorList["adios"] == undefined){
+            console.log(indicadorList["hola"]);
+        }
+        */
     });
 }
