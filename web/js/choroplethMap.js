966220b50e26dde48b194f7b1cca6aa0a2c47c39
Sergio García <garciparedes@gmail.com>
2015-12-10 12:47:19 +0100

    chroplethMap in div

--- web/js/choroplethMap.js
+++ web/js/choroplethMap.js
@@ -3,7 +3,7 @@ function generateChoroplethMap(){
 
     d3.xml("components/map_cyl.svg", function(xml) {
 
-        document.body.appendChild(xml.documentElement);
+        document.getElementById("choroplethMap").appendChild(xml.documentElement);
 
         var elem2 = document.getElementById('leon');
         elem2.style.fill = d3.rgb(31,119,180);
