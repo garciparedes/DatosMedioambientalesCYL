8c32f35ec772df7d8b91b46a53ffa9c35901dc43
Sergio García <garciparedes@gmail.com>
2015-12-12 22:47:59 +0100

    trying to resize svg file

--- web/js/choroplethMap.js
+++ web/js/choroplethMap.js
@@ -1,18 +1,49 @@
 
 function generateChoroplethMap(){
 
-    d3.xml("components/map_cyl.svg", function(xml) {
+    var dataset = [ 1];
 
-        document.getElementById("choroplethMap").appendChild(xml.documentElement);
 
-        var elem2 = document.getElementById('leon');
+    //Width and height
+    var w = 1000;
+    var h = 1000;
+
+
+
+
+    //Import the svgMap
+    d3.xml("components/map_cyl.svg", "image/svg+xml", function(xml) {
+        var importedNode = document.importNode(xml.documentElement, true);
+        var svgMap = document.getElementById("choroplethMap")
+            .appendChild(importedNode.cloneNode(true));
+
+        svgMap.setAttribute("width", "500px");
+        svgMap.setAttribute("height", "300px");
+        svgMap.setAttribute("style", "background-color red");
+        svgMap.setAttribute("height", "300px");
+
+
+        var elem2 = svgMap.getElementById('leon');
         elem2.style.fill = d3.rgb(31,119,180);
 
-        var elem3 = document.getElementById('valladolid');
+        var elem3 = svgMap.getElementById('valladolid');
         elem3.style.fill = d3.rgb(31,119,180);
 
-        var elem3 = document.getElementById('soria');
+        var elem3 = svgMap.getElementById('soria');
         elem3.style.fill = d3.rgb(31,119,180);
+
+        /*
+      d3.select(svgMap).select("path").attr("fill", "blue");
+      d3.select(svgMap).select("svg4146").attr("transform", function(d, i){
+          return "translate(" + (i * (w / dataset.length)) + ","
+              + (h - d*4 - (w )) + ")"
+              +"scale("+ 0.5 +")";
+      });
+      */
+
+
 
     });
+
+
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,4 @@
 function mainJS(){
     generateChoroplethMap();
-    generateSankeyDiagram();
+    //generateSankeyDiagram();
 }
