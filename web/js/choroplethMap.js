ca8351195dd60c9a2d8114402be1f70a5b3fe38b
Sergio García <garciparedes@gmail.com>
2015-12-15 10:22:22 +0100

    Now province's colours can be changed #5

--- web/js/choroplethMap.js
+++ web/js/choroplethMap.js
@@ -1,23 +1,19 @@
 
-function generateChoroplethMap(){
 
-    var provincias = [
-        'leon'
-        , 'palencia'
-        , 'burgos'
-        , 'salamanca'
-        , 'valladolid'
-        , 'soria'
-        , 'zamora'
-        , 'avila'
-        , 'segovia'
-    ];
-    var dataset = [ 1];
 
+function getMapPallete(){
+    return d3.scale.linear()
+        .domain([-1, 0, 1])
+        .range(["orange", "white", "blue"]);
+}
+
+
+function generateChoroplethMap(provincias){
 
     //Width and height
     var w = 400;
     var h = 325;
+
 
     //Create SVG Element
     var svg = d3.select("#choroplethMap")
@@ -26,18 +22,33 @@ function generateChoroplethMap(){
         .attr("height", h)
         .attr("viewBox", "0 0 800 650");
 
-    var color = d3.scale.linear()
-        .domain([-1, 0, 1])
-        .range(["orange", "white", "blue"]);
     //Import the plane
     d3.xml("components/map_cyl.svg", "image/svg+xml", function(xml) {
         var importedNode = document.importNode(xml.documentElement, true);
-
         svg.node().appendChild(importedNode);
 
         for ( i = 0; i < provincias.length; i++){
-            importedNode.getElementById(provincias[i]).style.fill = color(Math.random()*2-1);
+            setAttr(provincias[i]);
         }
-
     });
+}
+
+
+function setAttr(provincia){
+    updateMapColorProvince(provincia);
+    setOnClick(provincia);
+}
+
+
+function setOnClick(provincia){
+    d3.select("#" + provincia.Name)
+        .on("click",function(){
+            update(provincia);
+    });
+}
+
+
+function updateMapColorProvince(provincia){
+    d3.select("#" + provincia.Name)
+        .style( "fill", getMapPallete()(provincia.Index));
 }
